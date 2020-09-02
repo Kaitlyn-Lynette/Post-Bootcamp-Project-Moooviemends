47aee07fd41e3baf9b7f4741fc25fcf4fad95037
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-09-02 10:31:59 -0400

    Ran build folder

---
+++ client/build/precache-manifest.6cd0b864e8d55a759a5d05e08de797cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f976d778b59d96c7a8d1f488db1f15f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58bed7743bbe9dacf9ac",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e9ab2e3b2a67fad9050",
+    "url": "/static/js/2.455445ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.455445ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58bed7743bbe9dacf9ac",
+    "url": "/static/js/main.31ce216f.chunk.js"
+  },
+  {
+    "revision": "b5a8fa7d28925953cc2b",
+    "url": "/static/js/runtime-main.67cdb4ea.js"
+  },
+  {
+    "revision": "1b672e7ea2b56a6d0cea7329cd3c4794",
+    "url": "/static/media/logo.1b672e7e.png"
+  }
+]);
