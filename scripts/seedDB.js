cfbd06b5c8d6d7c99e017539a3b91e5ec34f574c
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-09-10 19:36:44 -0400

    Added seed data for playlist collection

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -21,9 +21,35 @@ const movieSeed = [
     },
 ];
 
+const playlistSeed = [
+    {
+        title: "Cartoon movies",
+        description: "Love me some cartoons!",
+    },
+    {
+        title: "Documentries",
+        description: "When I wanna learn somethin'",
+    },
+    {
+        title: "Throwbacks",
+        description: "Childhood favorites",
+    },
+];
+
+
+db.Movie.remove({})
+.then(() => db.Movie.collection.insertMany(movieSeed))
+.then((data) => {
+    console.log(data.result.n + ' records inserted');
+    process.exit(0);
+})
+.catch((err)=>{
+    console.log(err);
+    process.exit(1);
+});
 
 db.Playlist.remove({})
-.then(() => db.Playlist.collection.insertMany(movieSeed))
+.then(() => db.Playlist.collection.insertMany(playlistSeed))
 .then((data) => {
     console.log(data.result.n + ' records inserted');
     process.exit(0);
