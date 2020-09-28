7bd950ecd930fbb5ced5d8621491b6622173babb
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-09-28 16:52:29 -0400

    User can select a playlist and navigates to that playlist page

--- client/src/pages/Playlist.js
+++ client/src/pages/Playlist.js
@@ -6,11 +6,8 @@ import API from '../utils/API';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -63,7 +60,7 @@ export default function Playlist () {
             return (
                 <ListItem 
                 button={playlist._id}>
-                <ListItemLink href="#simple-list">
+                <ListItemLink href={"/playlists/" + playlist._id}>
                 <ListItemText>{playlist.title}</ListItemText>
                 <ListItemText>{playlist.description}</ListItemText>
                 </ListItemLink>
@@ -72,7 +69,6 @@ export default function Playlist () {
             })}
         </List>
         </Grid>
-    
         </Grid>
     )
 }
