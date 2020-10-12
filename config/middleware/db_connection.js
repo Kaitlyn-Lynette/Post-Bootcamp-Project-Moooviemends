602697be4bade216e2532d9be0ca950e5b198f40
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-10-12 15:51:35 -0400

    Added database credentials and environment variables

--- config/middleware/db_connection.js
+++ config/middleware/db_connection.js
@@ -1,10 +1,14 @@
 //Connect to Mongo database
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const dotenv = require("dotenv");
+dotenv.config()
 
 //your local database url
 //27017 is the default mongoDB port
-var uri = process.env.MONGODB_URI || 'mongodb://localhost/moooviemends';
+// var uri = process.env.MONGODB_URI || 'mongodb://localhost/moooviemends';
+
+var uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@moooviemends.ihnri.mongodb.net/moooviemends?retryWrites=true&w=majority` || 'mongodb://localhost/moooviemends'
 
 mongoose.connect(uri,
   {
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ const router = require('express').Router();
 const apiRoutes = require ('./api/')
 
 
+
 router.use('/api', apiRoutes);
 
 router.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
   
+
+
 module.exports = router;
